f23f31a774ac833105310c83687cc4afb8b7f0ac
Nirpendra Chaudhary <55339698+Nirpendra09@users.noreply.github.com>
2025-01-10 05:02:55 +0000

    Day 7

---
+++ 187. Repeated DNA Sequences.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> set = new HashSet<>();
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < s.length() - 9; i++) {
+             String str = s.substring(i, i + 10);
+             if(map.containsKey(str)) {
+                set.add(str);
+             } else {
+                map.put(str, 1);
+             }
+        }
+
+        List<String> list = new ArrayList<>(set);
+        return list;
+    }
+}
