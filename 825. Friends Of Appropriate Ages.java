4b5d2e969ba69d2c1fd4f971c267ab4615a93d01
Nirpendra Chaudhary <55339698+Nirpendra09@users.noreply.github.com>
2025-01-11 14:59:50 +0000

    Day 8

---
+++ 825. Friends Of Appropriate Ages.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int n = s.length();
+
+        int maxLen = 0;
+        int start = 0;
+        int curCost = 0;
+
+        for(int i = 0; i < n; i++) {
+            curCost += Math.abs(s.charAt(i) - t.charAt(i));
+
+            while(curCost > maxCost) {
+                curCost -= Math.abs(s.charAt(start) - t.charAt(start));
+                start++;
+            }
+
+            maxLen = Math.max(maxLen, i - start + 1);
+        }
+        
+        return maxLen;
+    }
+}
